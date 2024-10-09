6fa8b649d725e445b85700a171327d7d2da5c023
ceobaig2023 <ceobaig2023@gmail.com>
2024-10-09 21:36:55 +0530

    updated code

--- EmployeeDepartmentManagementSystem/src/main/java/com/example/demo/entity/Employee.java
+++ EmployeeDepartmentManagementSystem/src/main/java/com/example/demo/entity/Employee.java
@@ -24,4 +24,45 @@ public class Employee {
     private Department department;
 
     // Getters and Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
